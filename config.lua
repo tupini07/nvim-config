dcee6a18c9d7ca76cf947cf12117f753f57afc1b
Andrea Tupini <tupini07@gmail.com>
2023-08-31 17:00:12 +0200

    Automatic commit - Thu Aug 31 17:00:12 WEDT 2023

--- config.lua
+++ config.lua
@@ -124,6 +124,7 @@ lvim.builtin.treesitter.highlight.enable = true
 -- vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright" })
 -- local opts = {} -- check the lspconfig documentation for a list of all possible options
 -- require("lvim.lsp.manager").setup("pyright", opts)
+require("lvim.lsp.manager").setup("fennel_ls", {})
 
 -- ---remove a server from the skipped list, e.g. eslint, or emmet_ls. !!Requires `:LvimCacheReset` to take effect!!
 -- ---`:LvimInfo` lists which server(s) are skipped for the current filetype
